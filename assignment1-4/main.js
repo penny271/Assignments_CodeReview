3c6b7dac2a1683f554d01a64c48145f025d0213b
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-06-13 22:27:25 +0900

    課題1-4、オブジェクトの理解が中途半端で難航しましたが、完了しました！ 至らない点等、ご指摘お願いいたします。

--- assignment1-4/main.js
+++ assignment1-4/main.js
@@ -1,3 +1,40 @@
 "use strict";
 
+{
+  function lists() {
+    console.log("===================");
+    console.log("現在持っているタスク一覧");
+    console.log("===================");
+  }
 
+   lists();
+
+   const content = "【内容】";
+   const genre = "【ジャンル】";
+
+   const tasks1 = {
+     0: `${content}机を片付ける、${genre}掃除`,
+     1: `${content}牛乳を買う、${genre}買い物`,
+     2: `${content}散歩する、${genre}運動`,
+   };
+
+   function adds() {
+      Object.keys(tasks1).forEach((task) => {
+         console.log(`${task} : ${tasks1[task]}`); 
+      });
+   }
+
+   adds();
+
+  const question1 = prompt("タスクを入力してください");//*popup1
+  const question2 = prompt("ジャンルを入力してください");//*popup2 NEW
+  alert("新しいタスクを追加しました");//*popup3
+
+   console.log(tasks1);
+
+  lists();
+
+  adds();
+
+  prompt("「確認、追加、削除、修了」のいずれかを入力してください");//*popup4
+}
