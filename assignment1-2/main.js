97f6aa4de563995c1cb24cfc3d9ecb2fb35d9826
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-06-13 15:33:42 +0900

    課題1-2完了！

--- assignment1-2/main.js
+++ assignment1-2/main.js
@@ -1,9 +1,27 @@
 "use strict";
 
-{
-   // prompt('good morning!'); 
-}
- 
-console.log('AAAAAA');
+// {
+//   while (1) {
+//     const question = prompt("日本の首都は?");
+//     const answer = "東京";
+//     if (question === answer) {
+//       alert("正解です！");
+//     } else {
+//       alert("不正解です！");
+//     }
+//   }
+// }
 
-  
+{
+let i = 0;
+
+  do {
+    const question = prompt("日本の首都は?");
+    const answer = "東京";
+    if (question === answer) {
+      alert("正解です！");
+    } else {
+      alert("不正解です！");
+    }
+  } while (i===0);
+}
