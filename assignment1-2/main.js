e62c69aa4b14273442b20c5294f35de317bdc58d
penny <59471379+penny271@users.noreply.github.com>
2020-06-13 16:04:00 +0900

    Update main.js

--- assignment1-2/main.js
+++ assignment1-2/main.js
@@ -1,21 +1,7 @@
 "use strict";
 
-// {
-//   while (1) {
-//     const question = prompt("日本の首都は?");
-//     const answer = "東京";
-//     if (question === answer) {
-//       alert("正解です！");
-//     } else {
-//       alert("不正解です！");
-//     }
-//   }
-// }
-
 {
-let i = 0;
-
-  do {
+  while (1) {
     const question = prompt("日本の首都は?");
     const answer = "東京";
     if (question === answer) {
@@ -23,5 +9,6 @@ let i = 0;
     } else {
       alert("不正解です！");
     }
-  } while (i===0);
+  }
 }
+
