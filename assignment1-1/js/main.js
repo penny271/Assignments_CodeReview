0149e6c327b26441d2abc8d8a683f9119f4ee223
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-06-13 11:43:24 +0900

    課題１、完了しました。

--- assignment1-1/js/main.js
+++ assignment1-1/js/main.js
@@ -1,2 +1,14 @@
-'use strict';
+"use strict";
+
+{
+   for (let i = 3; i <= 99; i++) {
+      if (i % 15 === 0) {
+         console.log(`FizzBuzz ${i}`);
+      } else if (i%5===0) {
+         console.log(`Buzz ${i}`);
+      } else if (i % 3 === 0) {
+         console.log(`Fizz ${i}`);
+      }
+   }
+}
 
