91bd8238745de65cd22f2777489b37d5162b7dcb
hirotaka aoki <hirotaka.aoki27@gmail.com>
2020-06-15 06:23:00 +0900

    課題1-4のコードレビュー後再修正完了！

--- assignment1-4/main.js
+++ assignment1-4/main.js
@@ -1,40 +1,43 @@
 "use strict";
 
 {
-  function lists() {
-    console.log("===================");
-    console.log("現在持っているタスク一覧");
-    console.log("===================");
-  }
+   //consoleの内容をまとめて表示します。
+   const showlists = () => {
+      console.log("===================");
+      console.log("現在持っているタスク一覧");
+      console.log("===================");
+   }
 
-   lists();
+   showlists();
 
    const content = "【内容】";
    const genre = "【ジャンル】";
 
-   const tasks1 = {
-     0: `${content}机を片付ける、${genre}掃除`,
-     1: `${content}牛乳を買う、${genre}買い物`,
-     2: `${content}散歩する、${genre}運動`,
+   const originalTasks = { //変数に数字は使わないようにしましょう！！←修正しました
+      0: `${content}机を片付ける、${genre}掃除`,
+      1: `${content}牛乳を買う、${genre}買い物`,
+      2: `${content}散歩する、${genre}運動`,
    };
 
-   function adds() {
-      Object.keys(tasks1).forEach((task) => {
-         console.log(`${task} : ${tasks1[task]}`); 
+   //定数originalTasksの内容(現在持っているタスク一覧)を1行ずつコンソールに表示させます。
+   const showTasks = () => {
+      Object.keys(originalTasks).forEach((task) => {
+         console.log(`${task} : ${originalTasks[task]}`);
       });
    }
 
-   adds();
+   showTasks();
 
-  const question1 = prompt("タスクを入力してください");//*popup1
-  const question2 = prompt("ジャンルを入力してください");//*popup2 NEW
-  alert("新しいタスクを追加しました");//*popup3
+   const promptTask = prompt("タスクを入力してください"); //*popup1
+   const promptGenre = prompt("ジャンルを入力してください"); //*popup2 NEW
+   alert("新しいタスクを追加しました");//*popup3
 
-   console.log(tasks1);
+   originalTasks["3"] = `${content}${promptTask}${genre}${promptGenre}`;
 
-  lists();
+   showlists();
 
-  adds();
+   showTasks();
 
-  prompt("「確認、追加、削除、修了」のいずれかを入力してください");//*popup4
+   prompt("「確認、追加、削除、修了」のいずれかを入力してください"); //*popup4
 }
+
